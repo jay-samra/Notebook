963fc032b2f934cc09a16debb50db04135d223f9
Jagroop <jagroopsingh1936@gmail.com>
2025-08-12 15:00:07 -0700

    adding basic retrieve function

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const saveButton = document.getElementById("saveButton")
 const textArea = document.getElementById("entry")
+const entryButton = document.getElementById("entryButton")
+
+var journalNumbers = 1;
+localStorage.setItem("Journal Counter", journalNumbers)
 // Saving the value of the text box
 // Get value of text box -> retrieve local storage -> add entry to local storage -> clear text box
 function saveEntry(){
@@ -8,11 +12,19 @@ function saveEntry(){
     if (!text){
         return;
     }
-    localStorage.setItem("jorunal1", text)
-
+    localStorage.setItem('Journal ' + journalNumbers, text)
+    console.log("Saving " + journalNumbers)
+    journalNumbers++;
     textArea.value = ""
 
 
 }
+// Have 2 rereive functions, 1 for specific number and 1 for all entries
+function loadEntries(){
+    const entryText = localStorage.getItem('Journal ' + journalNumbers);
+    console.log("Retrieving " + journalNumbers)
+    console.log(entryText);
+}
 
 saveButton.addEventListener('click', saveEntry)
+entryButton.addEventListener('click', loadEntries)
